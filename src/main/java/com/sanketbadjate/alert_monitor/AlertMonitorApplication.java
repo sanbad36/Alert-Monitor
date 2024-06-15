7cd1d96ac481e51d429d117c89d836ffc32fb83b
sanbad36 <sanketbadjate36@gmail.com>
2024-06-15 22:39:25 +0530

    project setup

---
+++ src/main/java/com/sanketbadjate/alert_monitor/AlertMonitorApplication.java
@@ -0,0 +1,13 @@
+package com.sanketbadjate.alert_monitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlertMonitorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlertMonitorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sanketbadjate/alert_monitor/AlertMonitorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sanketbadjate.alert_monitor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlertMonitorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
